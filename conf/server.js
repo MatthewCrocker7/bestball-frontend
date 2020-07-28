274678074d7c078d7eab0534e6a28009c1174619
Matthew Crocker <matthewcroc@gmail.com>
2020-07-28 12:43:01 -0500

    Initial setup

---
+++ conf/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const path = require('path');
+const app = express();
+
+const publicPath = path.join(__dirname, '..', 'dist');
+
+app.use(express.static(publicPath));
+app.use(bodyparser.json());
+app.use(express.json());
+
+const port = process.env.PORT || 8080;
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Server started on port ', port, '!');
+});
